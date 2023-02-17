7e92ddef905bf0e34842ff6429ced81cb04110a1
Thibault <thibault.constans@outlook.com>
2023-02-17 18:01:09 +0100

    set apgination to comics page

--- src/page/comics.js
+++ src/page/comics.js
@@ -1,16 +1,25 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import ComicsList from "../Componant/ComicsList";
+import Limit from "../Componant/limitSelect";
+import Pagination from "../Componant/Pagination";
 
 const Comics = () => {
-  const [comics, setComics] = useState([]);
+  const [comics, setComics] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [pageCount, setPageCount] = useState();
+
+  const [limit, setLimit] = useState(10);
+  const [skip, setSkip] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/comics");
+        const response = await axios.get(
+          `http://localhost:3000/comics?limit=${limit}&skip=${skip * limit}`
+        );
         // console.log(response.data.results);
+        setPageCount(response.data);
         setComics(response.data.results);
         setIsLoading(false);
       } catch (error) {
@@ -18,14 +27,37 @@ const Comics = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [limit, skip]);
+  console.log(comics);
+  const handleLimitChange = (event) => {
+    setLimit(event.target.value);
+    setSkip(0);
+    // reset skip to 0 when limit changes
+  };
 
   return isLoading ? (
     <p>Loading...</p>
   ) : (
     <div style={{backgroundColor: "black"}}>
       <h1 style={{color: "lightgray"}}>Comics</h1>
+      <Limit limit={limit} handleLimitChange={handleLimitChange} />
+      <div>
+        <Pagination
+          limit={limit}
+          data={pageCount}
+          currentPage={skip}
+          setCurrentPage={setSkip}
+        />
+      </div>
       <ComicsList comics={comics} />
+      <div>
+        <Pagination
+          limit={limit}
+          data={pageCount}
+          currentPage={skip}
+          setCurrentPage={setSkip}
+        />
+      </div>
     </div>
   );
 };

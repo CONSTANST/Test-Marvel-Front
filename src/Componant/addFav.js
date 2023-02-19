0d81a415a3b9125202939e051dfa6172ff2ea19b
Thibault <thibault.constans@outlook.com>
2023-02-19 20:49:31 +0100

    add et remove Fav

---
+++ src/Componant/addFav.js
@@ -0,0 +1,8 @@
+const addFavorite = (id, type) => {
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || {};
+  favorites[type] = favorites[type] || [];
+  if (!favorites[type].includes(id)) {
+    favorites[type].push(id);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }
+};
---
+++ src/Componant/removeFav.js
@@ -0,0 +1,9 @@
+const removeFavorite = (id, type) => {
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || {};
+  favorites[type] = favorites[type] || [];
+  const index = favorites[type].indexOf(id);
+  if (index !== -1) {
+    favorites[type].slice(index, 1);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }
+};
